5fcb292481c1c846d0c345bb6f9082b2a689c3db
KUMAR Nirpendra <nirpendra.kumar@soprasteria.com>
2024-12-16 14:28:09 +0530

    Create hugo.yaml

---
+++ content/AKS/third-page.md
@@ -0,0 +1,5 @@
++++
+title = 'Third Page'
+date = 2024-12-16T12:49:13+05:30
+draft = true
++++
---
+++ public/searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "First Chapter",
+    "uri": "/first-chapter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "First Chapter",
+    "content": "TOC",
+    "description": "TOC",
+    "tags": [],
+    "title": "Kubernetes Installation",
+    "uri": "/first-chapter/k8s/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
