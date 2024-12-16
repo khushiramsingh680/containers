59f022795bc05365e4d48526b2d0f493b1b901e2
KUMAR Nirpendra <nirpendra.kumar@soprasteria.com>
2024-12-16 17:33:38 +0530

    Added

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -1,11 +1,11 @@
 var relearn_searchindex = [
   {
     "breadcrumb": "",
-    "content": "This is a new chapter.",
-    "description": "This is a new chapter.",
+    "content": "AKS allows you to quickly deploy a production ready Kubernetes cluster in Azure",
+    "description": "AKS allows you to quickly deploy a production ready Kubernetes cluster in Azure",
     "tags": [],
-    "title": "First Chapter",
-    "uri": "/first-chapter/index.html"
+    "title": "Azure Kubernetes Service (AKS)",
+    "uri": "/aks/index.html"
   },
   {
     "breadcrumb": "",
@@ -16,12 +16,12 @@ var relearn_searchindex = [
     "uri": "/categories/index.html"
   },
   {
-    "breadcrumb": "First Chapter",
+    "breadcrumb": "Azure Kubernetes Service (AKS)",
     "content": "TOC",
     "description": "TOC",
     "tags": [],
     "title": "Kubernetes Installation",
-    "uri": "/first-chapter/k8s/index.html"
+    "uri": "/aks/k8s/index.html"
   },
   {
     "breadcrumb": "",
